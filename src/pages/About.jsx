3537d5167825176a5dae56209611ec11acf0e1c3
Andrey Zagoruiko <2500mbytes@gmail.com>
2022-08-02 14:01:33 -0700

    final commit #2

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,7 +5,10 @@ function About() {
     <div>
             <h1 className="text-6xl mb-4">GitHub Finder</h1> 
             <p className="mb-4 text-2xl font-light">
-              A React app to perform search on Github
+              A React app to do basic search on Github
+            </p>
+            <p className='mb-4 text-2xl font-light'>
+              Created by <a className='font-semibold' href="https://twitter.com/andreyzagoruiko">Andrey Zagoruiko</a> during <a className="font-semibold" href="https://www.udemy.com/course/react-front-to-back-2022">this course</a>
             </p>
 
     </div>
