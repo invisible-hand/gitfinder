9f34197c707efa8b8ef5a688afb9c4b3a8071cf9
Andrey Zagoruiko <2500mbytes@gmail.com>
2022-08-02 13:45:39 -0700

    final commit #2

--- src/App.js
+++ src/App.js
@@ -3,23 +3,30 @@ import Home from "./pages/Home"
 import About from "./pages/About"
 import NotFound from "./pages/NotFound"
 import { GithubProvider } from './context/github/GithubContext'
-
+import { AlertProvider } from './context/alert/AlertContext'
+import Alert from './components/layout/Alert'
 
 import Navbar from "./components/layout/Navbar"
 import Footer from "./components/layout/Footer"
+import User from './pages/User'
 
 function App() {
   return (
     <GithubProvider>
+      <AlertProvider>
+
     <Router>
      <div className="flex flex-col justify-between h-screen">
       
       <Navbar />
 
       <main className=" container mx-auto px-3 pb-12">
+        <Alert />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/user/:login" element={<User />} />
+
           <Route path="/notfound" element={<NotFound />} />
           <Route path="/*" element={<NotFound />} />
 
@@ -33,6 +40,8 @@ function App() {
      </div>
       
     </Router>
+    </AlertProvider>
+
     </GithubProvider>
   );
 }
